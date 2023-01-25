49d16cae3f4c6a9bb4bc2e81b28b572dc9198480
sriramboddu <123585956+sriramboddu@users.noreply.github.com>
2023-01-26 00:31:50 +0530

    Update Streamlit_app.py
    Added new code

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -10,3 +10,8 @@ streamlit.text('🐔 Hard-boiled free range egg')
 streamlit.text('🥑 Avocado Toast')
 
 streamlit.header('🍌🍓Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
+
+Myfruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(myfruit_list)
